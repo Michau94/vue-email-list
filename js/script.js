45ff97098a8156a8b87abfb6b96055ca01256aa8
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-01 13:57:02 +0200

    generate random mail from api

--- js/script.js
+++ js/script.js
@@ -6,16 +6,27 @@ const root = new Vue({
 
     data: {
         randomMail: '',
+        mailList: []
     },
     methods: {},
     created() {
 
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((res) => {
-                let response = res.data.response
-                this.randomMail = response;
-            })
+        for (let i = 0; i < 10; i++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    let response = res.data.response
+
+                    this.mailList.push(response)
+
+
+                })
+
+        }
+
+        console.log(this.mailList);
+
     }
 
 })
