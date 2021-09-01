615b91e085e3e8afd524162a1d9a331f822fb1e5
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-01 13:47:11 +0200

    Vue + axios setup

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@ console.log('js ok');
 console.log(Vue);
 
 const root = new Vue({
+    el: '#root',
 
-    data: {},
+    data: {
+        randomMail: '',
+    },
     methods: {},
+    created() {
+
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                let response = res.data.response
+                this.randomMail = response;
+            })
+    }
 
 })
